3cec78a979d09ab2cd4e327d1427e6b78563a700
Manuel Gil <manuelgil@MacBook-Pro-de-Manuel.local>
2026-04-22 17:43:25 +0100

    chore: changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
